6984c2c916dadd9a07f01115f93c1dd34a9489c5
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-01-21 23:57:31 +0200

    Added webhook for the ticket purchase event

--- components/PurchaseTicket.tsx
+++ components/PurchaseTicket.tsx
@@ -8,6 +8,7 @@ import { Ticket } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import ReleaseTicket from "./ReleaseTicket";
+import { createStripeCheckoutSession } from "@/actions/createStripeCheckoutSession";
 
 
 
@@ -54,7 +55,25 @@ function PurchaseTicket({ eventId }: { eventId: Id<"events"> }) {
 
 
   // Create Stripe checkout...
-  const handlePurchase = async () => { };
+  const handlePurchase = async () => {
+
+    if (!user) return;
+
+    try {
+      setIsLoading(true);
+      const { sessionUrl } = await createStripeCheckoutSession({
+        eventId,
+      });
+
+      if (sessionUrl) {
+        router.push(sessionUrl);
+      }
+    } catch (error) {
+      console.error("Error creating checkout session:", error);
+    } finally {
+      setIsLoading(false);
+    }
+   };
 
   if (!user || !queuePosition || queuePosition.status !== "offered") {
     return null;
